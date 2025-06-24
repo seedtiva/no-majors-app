8edda1d12bd79b14a975834efe29d7b8df8f02e6
SEEDTIVA <seedtiva@SEEDTIVAs-iMac.local>
2025-06-23 20:32:45 -0400

    Setp: Expo + Tailwind + Fonts

--- eslint.config.js
+++ eslint.config.js
@@ -8,3 +8,36 @@ module.exports = defineConfig([
     ignores: ['dist/*'],
   },
 ]);
+export default {
+  expo: {
+    name: "No Majors",
+    slug: "no-majors-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#000000"
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#000000"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "your-project-id"
+      }
+    }
+  }
+}
